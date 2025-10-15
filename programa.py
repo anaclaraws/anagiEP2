48b441c8fa9e05cc59a228a183d92c2e7f80e826
Ana Clara <anacws1@al.insper.edu.br>
2025-10-15 18:22:04 -0300

    Questao 8 - correção

--- programa.py
+++ programa.py
@@ -1,4 +1,5 @@
 from funcoes import *
+
 frota = {
     "porta-aviões": [],
     "navio-tanque": [],
@@ -17,13 +18,23 @@ for nome, tamanho, quantidade in tipos:
     for i in range(quantidade):
         while True:
             print(f"Insira as informações referentes ao navio {nome} que possui tamanho {tamanho}")
+            
             linha = int(input("Linha: "))
+            while linha < 0 or linha > 9:
+                print("Linha inválida!")
+                linha = int(input("Linha: "))
+
             coluna = int(input("Coluna: "))
+            while coluna < 0 or coluna > 9:
+                print("Coluna inválida!")
+                coluna = int(input("Coluna: "))
+
             if tamanho == 1:
                 orientacao = "vertical"
             else:
                 op = int(input("[1] Vertical [2] Horizontal >"))
                 orientacao = "vertical" if op == 1 else "horizontal"
+
             if posicao_valida(frota, linha, coluna, orientacao, tamanho):
                 preenche_frota(frota, nome, linha, coluna, orientacao, tamanho)
                 break
@@ -44,19 +55,24 @@ jogadas_anteriores = []
 
 while True:
     print(monta_tabuleiros(tabuleiro_jogador, tabuleiro_oponente))
+    
     linha = int(input("Jogador, qual linha deseja atacar? "))
     while linha < 0 or linha > 9:
         print("Linha inválida!")
         linha = int(input("Jogador, qual linha deseja atacar? "))
+    
     coluna = int(input("Jogador, qual coluna deseja atacar? "))
     while coluna < 0 or coluna > 9:
         print("Coluna inválida!")
         coluna = int(input("Jogador, qual coluna deseja atacar? "))
+
     if [linha, coluna] in jogadas_anteriores:
         print(f"A posição linha {linha} e coluna {coluna} já foi informada anteriormente!")
         continue
+    
     jogadas_anteriores.append([linha, coluna])
     faz_jogada(tabuleiro_oponente, linha, coluna)
+    
     if afundados(frota_oponente, tabuleiro_oponente) == 10:
         print("Parabéns! Você derrubou todos os navios do seu oponente!")
         break
