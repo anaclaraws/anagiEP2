4962569fad20073cb62e968f624c7511de95326a
Ana Clara <anacws1@al.insper.edu.br>
2025-10-15 11:02:37 -0300

    Questao 2

--- funcoes.py
+++ funcoes.py
@@ -1,11 +1,22 @@
+#1
 def define_posicoes(linha, coluna, orientacao, tamanho):
     posicoes = []
     
     if orientacao == "vertical":
         for i in range(tamanho):
             posicoes.append([linha + i, coluna])
-    else:  # orientacao == "horizontal"
+    else:  
         for i in range(tamanho):
             posicoes.append([linha, coluna + i])
     
-    return posicoes
+    return posicoes 
+#2
+def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
+    posicoes = define_posicoes(linha, coluna, orientacao, tamanho)
+
+    if nome_navio not in frota:
+        frota[nome_navio] = [posicoes]
+    else:
+        frota[nome_navio].append(posicoes)
+    
+    return frota
